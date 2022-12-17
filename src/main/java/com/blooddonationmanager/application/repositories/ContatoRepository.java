20194a07839e4dd7c8fde29bd33825a8b509f9e1
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2022-12-17 19:51:30 -0300

    feat: Cria repositorios para as entidades

---
+++ src/main/java/com/blooddonationmanager/application/repositories/ContatoRepository.java
@@ -0,0 +1,11 @@
+package com.blooddonationmanager.application.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.blooddonationmanager.application.entities.Contato;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
+    
+}
