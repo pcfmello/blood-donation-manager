c4ab5df2b1788528274021d1c0317a7c0141c4c2
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2022-12-19 04:00:03 -0300

    feat: Configura o escaneamento de entidades

--- src/main/java/com/blooddonationmanager/application/Application.java
+++ src/main/java/com/blooddonationmanager/application/Application.java
@@ -2,8 +2,12 @@ package com.blooddonationmanager.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.blooddonationmanager.application.entities"})
 public class Application {
 
 	public static void main(String[] args) {
